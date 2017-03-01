bbfbd6506554f406017d9be0ad4a15722df5a191
zlxia0013 <zlxia0013@163.com>
2017-03-01 23:15:24 +0800

    mmmm

--- src/main/java/com/jack/doormis/core/clientquery/bo/ClientQueryBo.java
+++ src/main/java/com/jack/doormis/core/clientquery/bo/ClientQueryBo.java
@@ -3,6 +3,7 @@ package com.jack.doormis.core.clientquery.bo;
 
 import com.jack.doormis.core.clientquery.dao.ClientQueryDao;
 import com.jack.doormis.core.clientquery.pojo.ClientQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 public class ClientQueryBo  {
+	@Autowired
 	private ClientQueryDao clientQueryDao;
 
 
